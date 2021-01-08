d5e0f47dbefcba163014442bf0dae15c6b59b5bb
drx <onursurme@yahoo.com>
2021-01-08 12:24:17 +0300

    jekyll olmayan bir siteyi deniyorum

---
+++ others/index.php
@@ -0,0 +1,12 @@
+
+<html>
+<!-- yorum
+        yorum -->
+  <head>
+    <title>drx welcomes you!!</title>
+  </head>
+  <body>
+    <h6>Hello world!</h6>
+<img src="Spinning_Globe.gif">
+  </body>
+</html>
